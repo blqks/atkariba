f00c60577b18b1ed52c718d6b7cc8a3775536d8c
blqks <cinismatiss@gmail.com>
2023-04-21 18:52:01 +0300

    Initial commit

---
+++ 21-04-2023 testing.py
@@ -0,0 +1,70 @@
+from tkinter import *
+
+GARUMS = 400
+PLATUMS = 600
+logs = Tk()
+logs.title("Atkarību cirks")
+a = Canvas(logs, width=PLATUMS, height=GARUMS)
+
+bg = PhotoImage(file='C:/Users/matīss/Desktop/python/atkariba/main_background.png')
+a.create_image(0, 0, image=bg, anchor='nw')
+
+mainvir = PhotoImage(file='C:/Users/matīss/Desktop/python/atkariba/main_heading.png')
+a.create_image(300,200, image=mainvir)
+a.pack()
+
+info_canvas = Canvas(logs, width=PLATUMS, height=GARUMS)
+info_canvas.pack_forget()
+
+start_canvas = Canvas(logs, width = PLATUMS, height = GARUMS)
+start_canvas.pack_forget()
+
+vid_x = PLATUMS / 2
+vid_y = GARUMS / 2
+
+
+#info poga un info canva
+global info_text_atgriezties
+info_text_atgriezties=None
+info_background=PhotoImage(file='C:/Users/matīss/Desktop/python/atkariba/main_background.png')
+def infopoga():
+  info_canvas.create_image(0, 0, image=info_background, anchor='nw')
+  logs.title('informācija')
+  a.pack_forget()
+  info_canvas.pack()
+  info_text_start = info_canvas.create_text(PLATUMS - 200,GARUMS - 30,text='START',font=('Bahnschrift Condensed', 12,'bold'),tags=('start'))
+  global info_text_atgriezties
+  info_text_atgriezties = info_canvas.create_text(PLATUMS - 400,GARUMS - 30,text='ATGRIEZTIES',font=('Bahnschrift Condensed', 12, 'bold'),tags=('atgriezties'))
+ 
+  info_canvas.create_text(PLATUMS - 300, GARUMS - 350, text = "Spēles noteikumi", font = ("Bahnshrift Condensed", 20, "bold"))
+  info_canvas.create_text(PLATUMS - 305, GARUMS - 320, text = "Uzsākot spēli lietotājam tiek uzdots jautājums.", font = ("Bahnshrift Condensed", 10, "bold"))
+  info_canvas.create_text(PLATUMS - 295, GARUMS - 290, text = "Ja tiek izvēlēta pareiza atbilde, lietotājs met kauliņu un dodas uz priekšu", font = ("Bahnshrift Condensed", 10, "bold"))
+  info_canvas.create_text(PLATUMS - 305, GARUMS - 260, text = "par tik laukumiem.", font = ("Bahnshrift Condensed", 10, "bold"))
+  info_canvas.create_text(PLATUMS - 305, GARUMS - 220, text = "Ja tiek izvēlēta nepareiza atbilde, lietotājs paliek tajā pašā laukā.", font = ("Bahnshrift Condensed", 10, "bold"))
+def atgriezties():
+  logs.title('Atkarību cirks')
+  a.pack()
+  info_canvas.pack_forget()
+
+global info, start
+
+info_canvas.tag_bind('atgriezties', '<Button-1>', lambda event: atgriezties())
+info_canvas.tag_bind('start', '<Button-1>', lambda event: startpoga()) 
+
+info = a.create_text(PLATUMS-100,GARUMS-50, text="INFO")
+
+#no a canva uz info canvu(info poga)
+a.tag_bind(info, '<Button-1>', lambda event: infopoga())
+
+start = a.create_text(PLATUMS - 50, GARUMS - 50,  text="START")
+a.tag_bind(start, '<Button-1>', lambda event: info_start())
+#def kas aizver a canvu un atver start canvu(start poga)
+def startpoga():
+  a.pack_forget()
+  start_canvas.pack()
+  global info, start
+#def kas aizver info canvu un atver start canvu
+def info_start():
+  info_canvas.pack_forget()
+  start_canvas.pack()
+logs.mainloop()
