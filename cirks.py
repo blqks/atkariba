941241ae87451018448cf83a2bc15094a55f0938
NBuza <103209610+NBuza@users.noreply.github.com>
2023-05-17 17:31:41 +0300

    Update cirks.py

--- cirks.py
+++ cirks.py
@@ -211,112 +211,16 @@ tpoga = start_canvas.tag_bind('t', '<Button-1>', lambda event: mest())
 logs.mainloop()
 
 #jautajumi
-# What is addiction?
-# Which neurotransmitter is commonly associated with addiction?
-# What is the primary psychoactive component in tobacco?
-# Which substance is the most widely used psychoactive drug in the world?
-# What is the term used to describe a physiological and psychological dependence on a substance?
-# Which drug is derived from the opium poppy plant?
-# What is the primary active ingredient in marijuana?
-# Which substance is responsible for the stimulating effects of coffee and tea?
-# What is the primary psychoactive component in alcoholic beverages?
-# Which drug is commonly known as "ecstasy" or "Molly"?
-# What is the process of gradually reducing drug dosage to alleviate withdrawal symptoms?
-# Which neurotransmitter is associated with pleasure and reward in the brain?
-# What is the term used to describe the intense craving for a substance?
-# Which substance is the primary addictive component in cigarettes?
-# What is the most widely abused illicit drug in the United States?
-# What is the term used to describe the state of physical and psychological discomfort when a substance is withdrawn?
-# Which drug is commonly used as a central nervous system depressant?
-# What is the primary active ingredient in hallucinogenic mushrooms?
-# Which neurotransmitter is associated with the regulation of mood and pleasure?
-# What is the term used to describe a condition where a person is physically dependent on a substance but does not exhibit compulsive drug-seeking behavior?
-# Which drug is commonly used to treat heroin addiction?
-# What is the primary psychoactive component in chocolate?
-# Which substance is the most widely abused opioid painkiller?
-# What is the term used to describe the phenomenon where increasing amounts of a substance are needed to achieve the desired effect?
-# Which drug is commonly known as "crack"?
-# What is the primary active ingredient in inhalants?
-# Which neurotransmitter is associated with the "fight-or-flight" response?
-# What is the term used to describe the use of multiple substances simultaneously?
-# Which substance is the most widely used illicit stimulant?
-# What is the primary active ingredient in prescription sleeping pills?
-# Which drug is commonly known as "roofies" or the "date rape drug"?
-# What is the term used to describe the combination of two or more substances that results in a stronger effect than either substance alone?
-# Which neurotransmitter is associated with the regulation of sleep, mood, and appetite?
-# What is the term used to describe the use of a substance despite the negative consequences it causes?
-# Which substance is the most widely abused illicit hallucinogen?
-# What is the primary psychoactive component in energy drinks?
-# Which drug is commonly used as a prescription painkiller and is highly addictive?
-# What is the term used to describe a relapse back into substance use after a period of abstinence?
-# Which neurotransmitter is associated with memory and learning?
-# What is the term used to describe the use of a substance in larger amounts or over a longer period than intended?
-# Which substance is the most widely used illicit drug among young adults?
-# What is the primary active ingredient in over-the-counter cough medicine when used in large amounts?
-# Which drug is commonly used to treat attention deficit hyperactivity disorder (ADHD)?
-# What is the term used to describe a persistent desire or unsuccessful efforts to cut down or control substance use?
-# Which neurotransmitter is associated with the regulation of appetite and satiety?
-# What is the term used to describe the use of a substance to alleviate withdrawal symptoms?
-# Which substance is the most widely abused prescription opioid painkiller?
-# 48. What is the primary psychoactive component in betel nut, a stimulant commonly chewed in some parts of the world?
-
-# Which drug is commonly known as "angel dust"?
-# What is the term used to describe a state of intense physical and psychological distress experienced when a substance is not available?
-
+j1 = "Kāda ir viss izplatītākā atkarība pasaulē?"
+j2 = "Kāds bija vidējais alkohola patēriņš uz vienu cilvēku Latvijā 2022. gadā?"
+j3 = "Kāda ir viss biežāk lietotā narkotika Latvijā?"
+j4 = "Kas ir atkarība?"
 
 
 
 #atbildes
 
-# Addiction is a condition characterized by the compulsive use of a substance or engagement in a behavior despite negative consequences.
-# Dopamine is commonly associated with addiction.
-# Nicotine is the primary psychoactive component in tobacco.
-# Caffeine is the most widely used psychoactive drug in the world.
-# Dependence is the term used to describe a physiological and psychological reliance on a substance.
-# Heroin is derived from the opium poppy plant.
-# Tetrahydrocannabinol (THC) is the primary active ingredient in marijuana.
-# Caffeine is responsible for the stimulating effects of coffee and tea.
-# Ethanol is the primary psychoactive component in alcoholic beverages.
-# MDMA (methylenedioxymethamphetamine) is commonly known as "ecstasy" or "Molly."
-# Tapering is the process of gradually reducing drug dosage to alleviate withdrawal symptoms.
-# Dopamine is associated with pleasure and reward in the brain.
-# Craving is the intense desire or urge for a substance.
-# Nicotine is the primary addictive component in cigarettes.
-# Marijuana is the most widely abused illicit drug in the United States.
-# Withdrawal is the state of physical and psychological discomfort when a substance is discontinued.
-# Alcohol is commonly used as a central nervous system depressant.
-# Psilocybin is the primary active ingredient in hallucinogenic mushrooms.
-# Serotonin is associated with the regulation of mood and pleasure.
-# Physical dependence is a condition where a person is physically reliant on a substance but does not exhibit compulsive drug-seeking behavior.
-# Methadone is commonly used to treat heroin addiction.
-# Theobromine is the primary psychoactive component in chocolate.
-# Oxycodone is the most widely abused opioid painkiller.
-# Tolerance is the phenomenon where increasing amounts of a substance are needed to achieve the desired effect.
-# Crack cocaine is commonly known as "crack."
-# Volatile solvents (e.g., glue, paint thinner) are the primary active ingredients in inhalants.
-# Norepinephrine is associated with the "fight-or-flight" response.
-# Polydrug use is the use of multiple substances simultaneously.
-# Cocaine is the most widely used illicit stimulant.
-# Zolpidem is the primary active ingredient in prescription sleeping pills.
-# Rohypnol (flunitrazepam) is commonly known as "roofies" or the "date rape drug."
-# Synergism is the combination of two or more substances that results in a stronger effect than either substance alone.
-# Serotonin is associated with the regulation of sleep, mood, and appetite.
-# Substance abuse is the use of a substance despite the negative consequences it causes.
-# LSD (lysergic acid diethylamide) is the most widely abused illicit hallucinogen.
-# Caffeine is the primary psychoactive component in energy drinks.
-# Oxycodone is commonly used as a prescription painkiller and is highly addictive.
-# Relapse is a return to substance use after a period of abstinence.
-# Acetylcholine is associated with memory and learning.
-# Bingeing is the use of a substance in larger amounts or over a longer period than intended.
-# Marijuana is the most widely used illicit drug among young adults.
-# Dextromethorphan (DXM) is the primary active ingredient in over-the-counter cough medicine when used in large amounts.
-# Methylphenidate (Ritalin) is commonly used to treat attention deficit hyperactivity disorder (
-# ADHD).
-# 44. Craving is a persistent desire or unsuccessful efforts to cut down or control substance use.
-
-# Serotonin is associated with the regulation of appetite and satiety.
-# Self-medication is the use of a substance to alleviate withdrawal symptoms.
-# OxyContin (oxycodone) is the most widely abused prescription opioid painkiller.
-# Arecoline is the primary psychoactive component in betel nut.
-# Phencyclidine (PCP) is commonly known as "angel dust."
-# Withdrawal syndrome is a state of intense physical and psychological distress experienced when a substance is not available.
+a1 = ["Nikotīns", "Alkohols", "Narkotikas"]
+a2 = ["12,5", "15", "9,5"]
+a3 = ["Marihuāna", "Amfetamīni", "Ekstazī"]
+a4 = ["Hroniska slimība", "Īslaicīga garīga slimība"]
